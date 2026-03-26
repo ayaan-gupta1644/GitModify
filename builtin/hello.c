195eff6cb109246366989a11a3ae4e18d9f053b9
ayaan-gupta1644 <ayaangupta79080@gmail.com>
2026-03-26 23:29:28 +0530

    added a new command hello

---
+++ builtin/hello.c
@@ -0,0 +1,7 @@
+#include "builtin.h"
+#include "common.h"
+
+int cmd_hello(int argc, const char **argv, const char *prefix) {
+    printf("Hello, I am Ayaan and welcome to my custom Git build!\n");
+    return 0;
+}
